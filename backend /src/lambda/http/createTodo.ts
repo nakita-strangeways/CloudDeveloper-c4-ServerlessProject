6384b13d6383a48968f4a3217e2b47ffa16f3850
Nakita <nakita.strangeways@numerator.com>
2020-05-03 15:07:12 -0700

    Create Todo Function is working with Postman

--- backend /src/lambda/http/createTodo.ts
+++ backend /src/lambda/http/createTodo.ts
@@ -1,12 +1,35 @@
-// import 'source-map-support/register'
+import { APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as AWS  from 'aws-sdk'
+import * as uuid from 'uuid'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+const docClient = new AWS.DynamoDB.DocumentClient()
+const toDoTable = process.env.TODO_TABLE
 
-// import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+  const itemId = uuid.v4()
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-//   // TODO: Implement creating a new TODO item
-//   return undefined
-// }
+  const newItem = {
+    id: itemId,
+    ...newTodo
+  }
+
+  await docClient.put({
+    TableName: toDoTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
+}
