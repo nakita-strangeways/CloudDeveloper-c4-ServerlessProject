5cc6dd78dd2a71346fbef4fc025be93de5694115
Nakita <nakita.strangeways@numerator.com>
2020-05-04 09:11:04 -0700

    todo update in progress

--- backend /src/lambda/http/updateTodo.ts
+++ backend /src/lambda/http/updateTodo.ts
@@ -1,13 +1,34 @@
-// import 'source-map-support/register'
+import 'source-map-support/register'
+import * as AWS  from 'aws-sdk'
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-// import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-// import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
-//   const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+const docClient = new AWS.DynamoDB.DocumentClient()
+const toDoTable = process.env.TODO_TABLE
 
-//   // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-//   return undefined
-// }
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
+
+  const updatedItem = {
+    id: todoId,
+    ...updatedTodo
+  }
+  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
+  await docClient.put({
+    TableName: toDoTable,
+    Item: updatedItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      updatedItem
+    })
+  }
+}
