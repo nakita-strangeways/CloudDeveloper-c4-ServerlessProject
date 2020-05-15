0df38f69c29dda6c00c8375c72d6d6ff5ab42707
Nakita <nakita.strangeways@numerator.com>
2020-05-15 16:15:21 -0700

    first commit

--- client /src/config.ts
+++ client /src/config.ts
@@ -1,10 +1,10 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = "ey0wsk7b32"
+const apiId = "cgop860une"
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
   domain: 'dev-j79lyd38.auth0.com',            // Auth0 domain
-  clientId: '7QumchHsdLnE2GRTJmdTsII2LYLYkPII',          // Auth0 client id
+  clientId: 'YJKUK6r75qNeyj9tuQQ7Re4CUQfjJXQk',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
